91344b29041c1e51b22991de019cefc012a39514
DavidBoroznet <115796534+DavidBoroznet@users.noreply.github.com>
2023-06-20 17:30:19 +0300

    Update delete_agent.php

--- delete_agent.php
+++ delete_agent.php
@@ -10,14 +10,20 @@ if (isset($_GET['CodAgent']) && !empty($_GET['CodAgent'])) {
     // Получение CodAgent из URL
     $CodAgent = $_GET['CodAgent'];
 
-    // Подготовка и выполнение SQL-запроса на удаление агента
-    $deleteQuery = "DELETE FROM agent WHERE CodAgent = '$CodAgent'";
-    if ($mysqli->query($deleteQuery)) {
-        // Перенаправление на страницу со списком агентов
-        header("Location: agents.php");
-        exit();
+    // Подготовка и выполнение SQL-запроса на удаление всех квартир, привязанных к агенту
+    $deleteApartmentsQuery = "DELETE FROM Apartament WHERE CodAgent = '$CodAgent'";
+    if ($mysqli->query($deleteApartmentsQuery)) {
+        // Подготовка и выполнение SQL-запроса на удаление агента
+        $deleteAgentQuery = "DELETE FROM agent WHERE CodAgent = '$CodAgent'";
+        if ($mysqli->query($deleteAgentQuery)) {
+            // Перенаправление на страницу со списком агентов
+            header("Location: agents.php");
+            exit();
+        } else {
+            echo "Ошибка при удалении агента: " . $mysqli->error;
+        }
     } else {
-        echo "Ошибка при удалении агента: " . $mysqli->error;
+        echo "Ошибка при удалении квартир: " . $mysqli->error;
     }
 }
 
